0b824173334dc5d82c2c74c372ed07542521584f
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2023-10-27 17:19:48 -0600

    Started Order - Part1
    modified the following files:
        Index.razor - all POs are shown here
    
    Added the following files:
        index.razor.cs
        PurchaseOrderPage.razor
        PurchaseOrderPage.razor.cs

---
+++ BlazorPurchaseOrders/Pages/Index.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Navigations;
+using BlazorPurchaseOrders.Data;
+using Syncfusion.Blazor.Popups;
+using BlazorPurchaseOrders.Shared;
+using Syncfusion.Blazor.Grids;
+
+namespace BlazorPurchaseOrders.Pages {
+    public partial class Index : ComponentBase {
+
+        [Inject]
+        IPOHeaderService POHeaderService { get; set; }
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        IEnumerable<POHeader> poheader;
+        private List<ItemModel> Toolbaritems = new List<ItemModel>();
+
+        protected override async Task OnInitializedAsync() {
+            poheader = await POHeaderService.POHeaderList();
+
+            Toolbaritems.Add(new ItemModel() { Text = "Add", TooltipText = "Add a new order", PrefixIcon = "e-add" });
+            Toolbaritems.Add(new ItemModel() { Text = "Edit", TooltipText = "Edit a new order", PrefixIcon = "e-edit" });
+            Toolbaritems.Add(new ItemModel() { Text = "Delete", TooltipText = "Delete a new order", PrefixIcon = "e-delete" });
+        }
+
+        public void ToolbarClickHandler(Syncfusion.Blazor.Navigations.ClickEventArgs args) {
+            if (args.Item.Text == "Add") {
+                //Code for adding goes here
+            }
+            if (args.Item.Text == "Edit") {
+                //Code for editing
+            }
+            if (args.Item.Text == "Delete") {
+                //Code for deleting
+            }
+        }
+    }
+
+
+}
---
+++ BlazorPurchaseOrders/Pages/PurchaseOrderPage.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using BlazorPurchaseOrders.Data;
+
+namespace BlazorPurchaseOrders.Pages {
+    public partial class PurchaseOrderPage : ComponentBase {
+        [Inject] NavigationManager navigationManager { get; set; }
+
+        string pagetitle = "Add an Order";
+        [Parameter]
+        public int POHeaderID { get; set; }
+    }
+}
